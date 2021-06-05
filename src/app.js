8be397da7a1515883d290761599346b9c200ed7b
FuadAbdullah <muhammadfuadbinabdullah@gmail.com>
2021-06-05 12:18:33 +0800

    Purged old files

--- src/app.js
+++
@@ -1,9 +0,0 @@
-//TODO
-// Create playlist
-// Autoloop function
-
-const player = require('play-sound')(opts = {})
-
-player.play('./src/audio/test.mp3', { omxplayer: ['--vol', 550]}, function(err){
-    if (err) throw err
-  })
